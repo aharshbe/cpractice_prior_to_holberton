c5b2adf3a9f53e9c9e03432de7401c4941ff2b72
Austin <aharshbe@live.com>
2018-01-06 13:07:37 -0800

    adding data types folder for new data types

---
+++ C Programs/data_types/students.c
@@ -0,0 +1,69 @@
+/*
+	This program uses the header file
+	students.h to create a students type
+	it then asks the user 3 times to enter 
+	the students dorm, and name. It then
+	prents out the students details of 
+	those they entered.
+*/
+
+#include "students.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(int argc, char **argv){
+
+	int num_choice = 0;
+
+	printf("How many students would you like to create? : ");
+	scanf(" %d", &num_choice);
+
+	//Have to call getchar because of the use of fgets() below
+	getchar();
+	printf("\n");
+
+	//Creates an array of students
+	students students_arr[num_choice];
+
+	//Iterates over studnets in array to get their namne and dorm number
+	for(int i = 0; i < num_choice; i++){
+
+		//Allocates memeory to students name
+		students_arr[i].name = calloc(80, sizeof(char)*num_choice);
+
+		printf("What is the name of student %i? : ", i+1);
+		//Chose gets so I don't have to deal with extra return character i.e., \n
+		gets(students_arr[i].name);
+
+		//Allocates memeory to stduents dorm
+		students_arr[i].dorm = calloc(80, sizeof(char)*num_choice);
+
+		printf("What is the dorm number of the student? : ");
+		gets(students_arr[i].dorm);
+
+		puts("*********************************************\n");	
+
+	}
+
+	puts("******************Current dorm info******************\n");
+
+
+	//Iterates through array and prints student information
+	for(int k = 0; k < num_choice; k++){
+
+		printf("Studnet %i's name is %s and they will be in dorm %s.\n", k+1, students_arr[k].name, students_arr[k].dorm);
+	}
+
+	//Frees any allocated space that was used by calloc();
+	for(int j = 0; j < num_choice; j++){
+
+		free(students_arr[j].name);
+		free(students_arr[j].dorm);
+
+	}
+
+
+	return 0;
+
+}
---
+++ C Programs/data_types/students.h
@@ -0,0 +1,12 @@
+/*
+	This header file
+	creates a new data 
+	type called student
+*/
+
+typedef struct {
+
+	char *name;
+	char *dorm;
+
+} students;
